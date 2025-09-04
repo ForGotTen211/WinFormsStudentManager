ecd17c0a5b787824bd3ed13150bf8c6c91ec0e96
Admin <Admin@STEP-ADM-1>
2025-09-04 14:13:50 +0300

    Добавьте файлы проекта.

---
+++ StudentsManager/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsManager
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ StudentsManager/User.cs
@@ -0,0 +1,11 @@
+namespace StudentsManager;
+
+public class User
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public int Age { get; set; }
+    public int Group { get; set; }
+    public float AverageScore { get; set; }
+    public DateTime DateReceipt { get; set; }
+}
